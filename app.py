cf99ccba855309f6e4f5b85c8d73c6cc8e32c9f0
Richard Morrison <richard@rmorrison.net>
2020-03-19 20:42:00 +0000

    Tidy

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, Blueprint, jsonify
-from pynamodb.models import Model
-from pynamodb.attributes import UnicodeAttribute
-import os
 import datetime
+import os
+
+from flask import Blueprint, Flask, jsonify, render_template
 from flask_pynamodb_resource import create_resource
 from flask_restx import Api, Resource
+from pynamodb.attributes import UnicodeAttribute
+from pynamodb.models import Model
 
 IS_OFFLINE = bool(os.environ.get("IS_OFFLINE", False))
+
 
 class ShortCode(Model):
     class Meta:
@@ -15,6 +17,7 @@ class ShortCode(Model):
             host = "http://localhost:8000"
         else:
             region = os.environ["REGION"]
+
     group = UnicodeAttribute()
     viewed = UnicodeAttribute(null=True)
     code = UnicodeAttribute(hash_key=True)
@@ -23,11 +26,14 @@ class ShortCode(Model):
         for name, attr in self._get_attributes().items():
             yield name, attr.serialize(getattr(self, name))
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 @app.route("/<code>")
 def hit(code):
@@ -35,35 +41,39 @@ def hit(code):
         code = ShortCode.get(code)
         code.viewed = datetime.datetime.now().isoformat()
         code.save()
-        return render_template('thanks.html')
+        return render_template("thanks.html")
     except ShortCode.DoesNotExist:
         pass
-    return render_template('miss.html'), 404
-    
+    return render_template("miss.html"), 404
+
 
 # See https://github.com/brandond/flask-pynamodb-resource/blob/master/examples/office_model.py
 # Attach APIs to a blueprint to avoid cluttering up the root URL prefix
-api_bp = Blueprint('api_v1', __name__)
-api = Api(api_bp, default='Group', default_label='Groups', doc="/doc")
+api_bp = Blueprint("api_v1", __name__)
+api = Api(api_bp, default="Group", default_label="Groups", doc="/doc")
 
 # Create the resources and register them with the blueprint
 # Also override the endpoint name for each resource; by default it simply uses the model's table name
-create_resource(ShortCode).register(api, '/codes')
+create_resource(ShortCode).register(api, "/codes")
 
-@api.route('/groups/<id>')
-@api.param('id', _in="path")
+
+@api.route("/groups/<id>")
+@api.param("id", _in="path")
 class Group(Resource):
     def get(self, id):
-        codes = [dict(s) for s in ShortCode.scan(filter_condition=ShortCode.group==id)]
+        codes = [
+            dict(s) for s in ShortCode.scan(filter_condition=ShortCode.group == id)
+        ]
         return codes
 
+
 # Register the blueprint with the app AFTER attaching the resources
-app.register_blueprint(api_bp, url_prefix='/api/v1')
+app.register_blueprint(api_bp, url_prefix="/api/v1")
 
 if IS_OFFLINE:
     # Print routes to stdout
     for rule in app.url_map.iter_rules():
-        print('{} => {}'.format(rule.rule, rule.endpoint))
+        print("{} => {}".format(rule.rule, rule.endpoint))
 
 if __name__ == "__main__":
     app.run()
